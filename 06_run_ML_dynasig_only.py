92a93a994b67790febea245db18c074d6e514766
gregorpatof <shulgin@pop-os.localdomain>
2022-07-04 15:20:51 -0400

    all precomputed dsdfs

--- 06_run_ML_dynasig_only.py
+++ 06_run_ML_dynasig_only.py
@@ -1,7 +1,11 @@
 from dynasigml.dynasig_ml_model import DynaSigML_Model
+import json
 
 if __name__ == "__main__":
-    dsml_model = DynaSigML_Model("combined_dsdf.pickle", predictor_columns=["dynasig"])
+    with open("test_ids_8020.json") as f:
+        test_ids = json.load(f)
+    dsml_model = DynaSigML_Model("combined_dsdf.pickle", test_ids=test_ids, predictor_columns=["dynasig"])
     dsml_model.train_test_lasso()
     dsml_model.train_test_mlp()
+    dsml_model.make_graphs('graphs_folder_dynasig_only')
     dsml_model.save_to_file("dsml_dynasig_only")
--- precomputed_07_compute_dynasigs_double_mutants.py
+++ precomputed_07_compute_dynasigs_double_mutants.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     filenames_list = glob.glob("double_mutants/*.pdb")
     exp_data = [0 for x in filenames_list]
     exp_labels = ["dummy"]
-    dsml = load_dynasigml_model_from_file("dsml_dynasig_only")
+    dsml = load_dynasigml_model_from_file("dsml_dynasig_only.pickle")
     best_beta_values = dsml.get_best_beta_values()
     dsdf = DynaSigDF(filenames_list, exp_data, exp_labels, "dsdf_double_mutants", beta_values=best_beta_values)
