87a84b9215d6679d7cbe4be5359497134b5e2e5a
gregorpatof <shulgin@pop-os.localdomain>
2022-07-05 22:02:12 -0400

    almost ready

---
+++ supplementary_run_ML_static_properties.py
@@ -0,0 +1,14 @@
+from dynasigml.dynasig_ml_model import DynaSigML_Model
+import json
+
+if __name__ == "__main__":
+    with open("test_ids_8020.json") as f:
+        test_ids = json.load(f)
+    with open("vim2_exp_labels.json") as f:
+        exp_labels = json.load(f)
+    dsml_model = DynaSigML_Model("combined_dsdf.pickle", test_ids=test_ids, predictor_columns=exp_labels[1:])
+    dsml_model.train_test_lasso()
+    dsml_model.train_test_mlp()
+    dsml_model.make_graphs('graphs_folder_static_only')
+    dsml_model.save_to_file("dsml_static_only")
+
