5604d2177a442c02488f66268abc27b1ce3280ee
gregorpatof <shulgin@pop-os.localdomain>
2022-09-05 13:26:36 -0700

    before clean install

--- supplementary_run_ML_static_properties.py
+++ supplementary_run_ML_static_properties.py
@@ -12,3 +12,11 @@ if __name__ == "__main__":
     dsml_model.make_graphs('graphs_folder_static_only')
     dsml_model.save_to_file("dsml_static_only")
 
+    dsml_model = DynaSigML_Model("combined_dsdf.pickle", test_ids=test_ids, predictor_columns=["rosetta_ddg", "asa"])
+    dsml_model.train_test_lasso()
+    dsml_model.train_test_mlp()
+    dsml_model.make_graphs('graphs_folder_ddg_asa')
+    dsml_model.save_to_file("dsml_ddg_asa")
+
+
+
